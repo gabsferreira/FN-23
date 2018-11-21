f3d6f35b9f2b9750e4d9d818b4021813afc12a03
Gabriel <developer.gabriel@gmail.com>
2018-11-21 16:06:29 -0200

    Exercícios 1.15

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -9,15 +9,32 @@ namespace Blog.Controllers
 {
     public class PostController : Controller
     {
-        public IActionResult Index()
+        private IList<Post> listaDePosts;
+
+        public PostController()
         {
-            var listaDePosts = new List<Post>
+            this.listaDePosts = new List<Post>
             {
                 new Post { Titulo = "A volta dos que não foram", Resumo = "Uns foram e voltaram, outros não", Categoria = "Épico" },
                 new Post { Titulo = "Homem Aranha 3", Resumo = "Um homem que também é aranha", Categoria = "Heróis" },
                 new Post { Titulo = "Filme do Adam Sandler", Resumo = "Pode ser qualquer um", Categoria = "Pastelão" }
             };
+        }
+
+        public IActionResult Index()
+        {
             return View(listaDePosts);
+        }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        public IActionResult Adiciona (Post p)
+        {
+            this.listaDePosts.Add(p);
+            return View("Index", this.listaDePosts);
         }
     }
 }
