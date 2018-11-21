52e8794e04d2285f216321baedc7f38bacd786bc
Gabriel <developer.gabriel@gmail.com>
2018-11-21 12:08:46 -0200

    Exercícios 1.11

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            var listaDePosts = new List<Post>
+            {
+                new Post { Titulo = "A volta dos que não foram", Resumo = "Uns foram e voltaram, outros não", Categoria = "Épico" },
+                new Post { Titulo = "Homem Aranha 3", Resumo = "Um homem que também é aranha", Categoria = "Heróis" },
+                new Post { Titulo = "Filme do Adam Sandler", Resumo = "Pode ser qualquer um", Categoria = "Pastelão" }
+            };
+            ViewBag.Posts = listaDePosts;
+            return View();
+        }
+    }
+}
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public string Titulo { get; set; }
+        public string Resumo { get; set; }
+        public string Categoria { get; set; }
+    }
+}
